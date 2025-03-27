2bf3e42b8bb8d804eca7f57934124475760c3ef7
Sihun Kim <skim585@asu.edu>
2025-03-27 15:28:51 -0700

    feat(bank/): finish section 3, section 4
    finish section 3 and 4 which covers ownership proglems of rust.

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,3 +1,4 @@
+
 #[derive(Debug)]
 struct Account {
     id: u32,
@@ -15,6 +16,19 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+
+    fn add_account (&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+    fn total_balance (&self) -> i32{
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+    fn summary (&self) -> Vec<String>{
+        self.accounts
+        .iter()
+        .map(|account| account.summary())
+        .collect::<Vec<String>>()
+    }
 }
 
 impl  Account {
@@ -25,19 +39,37 @@ impl  Account {
             balance: 0
         }
     }
+
+    fn deposit (&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary (&self) -> String {
+        format!("{} has balance of {}", self.holder, self.balance)
+    }
 }
 
-fn print_account(account: Account) {
-    println!("{:#?}", account);
-}
 
 fn main() {
-    let bank = Bank::new();
-    // let account = Account::new(1, String::from("Pollito"));
-    // println!("{:#?}", bank);
-    // print_account(account);
-    let other_bank = bank;
-    let bank_2 = Bank::new();
-    println!("{:#?}", other_bank);
-    println!("{:#?}", bank_2);
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("Pollito"));
+    let mut account_2 = Account::new(2, String::from("Chiquito"));
+
+    account.deposit(500);
+    account.withdraw(250);
+    account_2.deposit(30);
+
+    // println!("{}", account.summary());
+
+    bank.add_account(account);
+    bank.add_account(account_2);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
