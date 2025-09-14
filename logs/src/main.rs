9beaf40c164a3adce62f191d92f72d52c628e2e4
Sihun Kim <skim585@asu.edu>
2025-09-14 16:16:31 -0700

    feat(logs/): wip on enums and error handling

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,7 +1,42 @@
 use std::fs;
+use std::io::Error;
 
 fn main() {
     let text = fs::read_to_string("logs.txt");
     println!("{:#?}", text);
     println!("Hello, world!");
+    match divide(5.0, 0.0) {
+        Ok(result_of_difision) => {
+            println!("{}", result_of_difision)
+        }
+        Err(what_went_wrong) => {
+            println!("{}", what_went_wrong)
+        }
+    }
+    println!("{:#?}", divide(5.0, 3.0));
+
+    match validate_email(String::from("asdf@gmail.com")) {
+        Ok(..) => print!("Email is valid"),
+        Err(reasion_this_failed_validation) => {
+            println!("{}", reasion_this_failed_validation)
+        }
+    }
+}
+
+fn validate_email (email: String) -> Result<(), Error> {
+    if email.contains("@") {
+        // success!
+        Ok(())
+    } else {
+        Err(Error::other("Emails must have an @"))
+    }
+}
+
+
+fn divide (a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("Can't divide by 0."))
+    } else {
+        Ok(a / b)
+    }
 }
